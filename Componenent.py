d0f940647d5c643468c33f9a065374d8a670a8b7
JUNGEEYOU <junge2u@gmail.com>
2019-11-15 11:46:11 +0900

    데코레이터 패턴 적용

--- Componenent.py
+++ Componenent.py
@@ -4,4 +4,4 @@ class NotifierComponenent:
      알림 Componenent 인터페이스
     """
     def send(self):
-        pass
+        raise NotImplementedError("Please Implement this method")
--- run.py
+++ run.py
@@ -2,8 +2,5 @@ from BasicNotify import BasicNotifier
 from Kakao import KakaoSender
 from Sms import SmsSender
 
-
-print("test")
-test = BasicNotifier()
 send_lena = KakaoSender(SmsSender(BasicNotifier(), "lena"), "lena")
 send_lena.send("error message")
